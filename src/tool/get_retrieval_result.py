df4a74b15ae8b31893b9e5c624b873e754c3403f
juncenli(李俊岑) <juncenli@tencent.com>
2018-03-29 00:23:09 +0800

    bugfix

--- src/tool/get_retrieval_result.py
+++ src/tool/get_retrieval_result.py
@@ -1,15 +1,15 @@
 import sys
 for i in range(2):
-	f=open('sentiment.test.'+str(i)+'.template.orgin.emb.result.filter.result.result','r')
+	f=open('sentiment.test.'+str(i)+'.template.orgin.emb.result.filter.result','r')
 	fw=open('sentiment.test.'+str(i)+'.retrieval','w')
 	tmp=''
 	tmp_array=[]
 	for line in f:
 		lines=line.strip().split('\t')
-		if(tmp!=lines[0] and tmp!=''):
+		if(tmp!=lines[1] and tmp!=''):
 			fw.write(tmp+'\t'+tmp_array[0]+'\t'+str(i)+'\n')
 			tmp_array=[lines[4]]
 		else:
 			tmp_array.append(lines[4])
-		tmp=lines[0]
+		tmp=lines[1]
 	fw.write(tmp+'\t'+tmp_array[0]+'\t'+str(i)+'\n')	
